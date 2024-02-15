7b8980fd9030f5a15c0039c5e7cc0a356682d4cb
Kevin Nguyen <KevinNguyen09161995@gmail.com>
2024-02-15 10:54:13 -0600

    Removed network to make it simplier

--- main.tf
+++ main.tf
@@ -6,3 +6,70 @@ terraform {
     }
   }
 }
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+
+#Subnets
+resource "aws_subnet" "public_us_east_2a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.10.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "public-us-east-2a"
+  }
+}
+
+resource "aws_subnet" "public_us_east_2b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.20.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "public-us-east-2b"
+  }
+}
+
+
+#Route Table Public Subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_us_east_2a" {
+  subnet_id      = aws_subnet.public_us_east_2a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_us_east_2b" {
+  subnet_id      = aws_subnet.public_us_east_2b.id
+  route_table_id = aws_route_table.public.id
+}
